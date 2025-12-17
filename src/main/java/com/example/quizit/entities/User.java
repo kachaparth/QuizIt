7f03b6174a59602db30721ad02c7abbdfd191d0a
kachaparth <kachaparth123@gmail.com>
2025-12-17 17:42:52 +0530

    Database connection problem

---
+++ src/main/java/com/example/quizit/entities/User.java
@@ -0,0 +1,54 @@
+package com.example.quizit.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID id;
+
+    @Column(name = "user_email", unique = true,length = 100)
+    private String email;
+    private String role;
+    private String username;
+    private String password;
+
+    private String image;
+
+    private boolean enable = true;
+
+
+    private Instant createdAt = Instant.now();
+    private Instant updatedAt = Instant.now();
+
+
+
+    @PrePersist
+    private void onCreate() {
+        Instant  now  = Instant.now();
+        if(this.createdAt == null) {
+            this.createdAt = now;
+        }
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
+
+
+}
---
+++ src/test/java/com/example/quizit/QuizItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
