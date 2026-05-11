97c091f21e52dcaf0070c49564b5c1cdd6c4053b
kachaparth <kachaparth123@gmail.com>
2026-05-12 00:34:45 +0530

    Health message update

--- backend/src/main/java/com/example/quizit/controllers/ApplicationController.java
+++ backend/src/main/java/com/example/quizit/controllers/ApplicationController.java
@@ -27,6 +27,6 @@ public class ApplicationController {
     public ResponseEntity<String> getHealth(){
 
         userRepository.count();// DB health
-        return ResponseEntity.ok("QuizIt is finally running!!!!!");
+        return ResponseEntity.ok("QuizIt is finally running...");
     }
 }
