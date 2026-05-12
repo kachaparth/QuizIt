5d1c7902d5815828b5816ea964e795c79b1ffc7a
kachaparth <kachaparth123@gmail.com>
2026-05-12 19:14:19 +0530

    chnage

--- backend/src/main/java/com/example/quizit/controllers/ApplicationController.java
+++ backend/src/main/java/com/example/quizit/controllers/ApplicationController.java
@@ -27,6 +27,6 @@ public class ApplicationController {
     public ResponseEntity<String> getHealth(){
 
         userRepository.count();// DB health
-        return ResponseEntity.ok("QuizIt is running...");
+        return ResponseEntity.ok("QuizIt");
     }
 }
